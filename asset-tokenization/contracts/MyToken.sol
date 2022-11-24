346117723a17717a29f09049be5f0832c4aeae6f
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2022-11-23 21:49:28 -0600

    [ New structure - openzeppelin ]

---
+++ asset-tokenization/contracts/MyToken.sol
@@ -0,0 +1,12 @@
+// contracts/MYToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract MYToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("StarDucks Cappucino Token", "CAPPU") {
+        _mint(msg.sender, initialSupply);
+    }
+}
